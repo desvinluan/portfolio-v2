b2519e2499202691b173aa4d512482726dc1daa7
desvinluan <deserrevinluan19@gmail.com>
2026-07-05 13:57:04 +0800

    finish hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import BannerImage from "./assets/images/banner.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,20 @@ function App() {
   return (
     <>
       <Header />
+      <main>
+        <section className="flex flex-col items-center justify-center py-4">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-4xl font-bold text-center max-w-2xl text-primary">
+              Product designer with a love for clean UI and low tolerance for
+              bad UX.
+            </h1>
+            <img src={BannerImage} alt="Banner" className="h-auto w-228" />
+            <p className="text-lg text-center text-muted-foreground max-w-xl font-body font-semibold">
+              I blend logic, aesthetics, and user empathy to create intuitive experiences that feel effortless and intentional.
+            </p>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
