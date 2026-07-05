7b6bfb54a83e0d66aa5031c2740b2259d6de79ea
desvinluan <deserrevinluan19@gmail.com>
2026-07-05 19:21:42 +0800

    do the bg for 3rd section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import BannerImage from "./assets/images/banner.png";
 import Me from "./assets/images/me.png";
 import Rab from "./assets/images/rab.png";
+import BgImage from "./assets/images/bg-section.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -26,10 +27,10 @@ function App() {
             </p>
           </div>
         </section>
-        <section className="flex justify-center bg-secondary h-220 pt-16">
+        <section className="flex justify-center bg-secondary h-228 pt-16">
           <div className="flex flex-col items-center justify-center gap-14">
             <img src={Me} alt="Me" className="h-auto w-120" />
-            <div className="flex flex-col items-center justify-center gap-40">
+            <div className="flex flex-col items-center justify-center gap-38">
               <p className="text-lg text-center text-muted-foreground max-w-2xl font-body font-semibold">
                 I’ve always been drawn to design, noticing how some apps feel
                 effortless while others make simple tasks harder than they need
@@ -43,6 +44,12 @@ function App() {
               <img src={Rab} alt="Me" className="h-auto w-64" />
             </div>
           </div>
+        </section>
+        <section className="bg-size-[1580px] bg-center w-full h-680"
+        style={{ backgroundImage: `url(${BgImage})` }}>
+          <div className="px-12 py-20">
+        <h1 className="text-primary text-4xl">see what i’ve been working on :)</h1>
+        </div>
         </section>
       </main>
     </div>
